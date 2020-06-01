b15acb62ee2ba5709ba4cd5db0419b7899093b72
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-01 12:05:22 +0200

    EmailController CRUD added

--- src/main/java/pl/coderslab/controller/EmailController.java
+++ src/main/java/pl/coderslab/controller/EmailController.java
@@ -2,12 +2,14 @@ package pl.coderslab.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import pl.coderslab.model.EmailMessage;
+import pl.coderslab.dto.EmailMessageDto;
+import pl.coderslab.errorhandler.exception.EntityNotFoundException;
 import pl.coderslab.service.EmailService;
 
 import javax.validation.Valid;
 
 @RestController
+@RequestMapping("/email")
 public class EmailController {
 
     private final EmailService emailService;
@@ -18,9 +20,20 @@ public class EmailController {
     }
 
     @PostMapping("/sendEmail")
-    public boolean sendEmail(@Valid @RequestBody EmailMessage emailMessage,
-                            @RequestParam("recipientEmail") String recipientEmail) {
-        return emailService.sendEmail(emailMessage, recipientEmail);
+    public boolean sendEmail(@Valid @RequestBody EmailMessageDto emailMessageDto,
+                             @RequestParam("recipientEmail") String recipientEmail,
+                             @RequestParam("senderEmail") String senderEmail) {
+        return emailService.sendEmail(emailMessageDto, recipientEmail, senderEmail);
+    }
+
+    @GetMapping("/find/{id}")
+    public EmailMessageDto findEmailMessageById(@PathVariable("id") Long emailId) throws EntityNotFoundException {
+        return emailService.findById(emailId);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public boolean removeEmailById(@PathVariable("id") Long emailId) throws EntityNotFoundException {
+        return emailService.removeById(emailId);
     }
 
 }
