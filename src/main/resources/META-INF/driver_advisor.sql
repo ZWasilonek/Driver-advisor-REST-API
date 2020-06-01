86bf0f0a793c76ae6cec4f87119651948af106e7
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-06-01 12:04:27 +0200

    Default user and admin added

--- src/main/resources/META-INF/driver_advisor.sql
+++ src/main/resources/META-INF/driver_advisor.sql
@@ -1,3 +1,8 @@
-create database driver_advisor char set utf8mb4 collate utf8mb4_unicode_ci;
+# create database driver_advisor char set utf8mb4 collate utf8mb4_unicode_ci;
 
-INSERT INTO roles VALUES (null, 'ROLE_USER'), (null, 'ROLE_ADMIN');
+INSERT INTO roles VALUES (1, 'ROLE_USER'), (2, 'ROLE_ADMIN');
+
+INSERT INTO users(enabled, password, username, email) VALUES (1,'$2a$10$jklI9ReiNRA4wSW6pNfKB.WrntRF0VihqXPCYX5OpEqQRAjG3OvFO', 'admin', 'admin@gmail');
+INSERT INTO users(enabled, password, username, email) VALUES (1,'$2a$10$kdadbv4MyqzKYKCSmE41Xu7hAc2z/Vr1n88lZwhAYq1zHvno7AfAe', 'user', 'user@gmail');
+
+INSERT INTO user_role VALUES (1, 2), (2, 1);
