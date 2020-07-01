f26e757902abe935b1117bfe988c727112cce333
Zofia Wasilonek <zofiawasilonek@gmail.com>
2020-07-01 15:54:55 +0200

    application.properties and DatabaseConfig

--- src/main/java/pl/coderslab/config/DatabaseConfig.java
+++ src/main/java/pl/coderslab/config/DatabaseConfig.java
@@ -1,27 +1,27 @@
-package pl.coderslab.config;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import javax.sql.DataSource;
-
-/**
- * This class is for HEROKU
-**/
-
-@Configuration
-public class DatabaseConfig {
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
-    }
-
-}
+//package pl.coderslab.config;
+//
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import javax.sql.DataSource;
+//
+///**
+// * This class is for HEROKU
+//**/
+//
+//@Configuration
+//public class DatabaseConfig {
+//
+//    @Value("${spring.datasource.url}")
+//    private String dbUrl;
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        HikariConfig config = new HikariConfig();
+//        config.setJdbcUrl(dbUrl);
+//        return new HikariDataSource(config);
+//    }
+//
+//}
